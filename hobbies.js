1509e5552ccfde9a319163d96db6e779fc3d8017
Efestus31 <biagiohd@gmail.com>
2024-10-29 17:33:04 +0100

    test funzione add export funzione

--- hobbies.js
+++ hobbies.js
@@ -1,3 +1,11 @@
-function hobbyList (hobbyOne, hobbyTwo, hobbyThree){
-    
+function hobbiesList(hobbyOne, hobbyTwo, hobbyThree){
+    this.hobbyOne = hobbyOne;
+    this.hobbyTwo = hobbyTwo;
+    this.hobbyThree = hobbyThree;
 }
+
+const hobbyList = new hobbiesList("lettura", "ciclismo", "cucina")
+console.log(hobbyList);
+
+//export della funzione
+module.exports = hobbiesList;
